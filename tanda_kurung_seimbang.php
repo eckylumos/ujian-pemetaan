7d09dec98bac477da6e76943f6a8181c3ff45d1e
eckylumos <ecky.erdhyka@lumoshive.com>
2025-08-29 15:14:51 +0700

    Update tanda_kurung_seimbang.php

--- tanda_kurung_seimbang.php
+++ tanda_kurung_seimbang.php
@@ -1,29 +1,45 @@
 <?php
 
-function isValid(string $s): bool
-{
-    if (strlen($s) % 2 !== 0) {
-        return false;
-    }
-
-    $stack = new SplStack();
-    $map = [
-        ')' => '(',
-        ']' => '[',
-        '}' => '{',
-    ];
-
-    for ($i = 0; $i < strlen($s); $i++) {
-        $char = $s[$i];
-        if (in_array($char, ['(', '[', '{'])) {
-            $stack->push($char);
-        } else if (in_array($char, [')', ']', '}'])) {
-            if ($stack->isEmpty() || $stack->pop() !== $map[$char]) {
-                return false;
-            }
-        }
-    }
-
-    return $stack->isEmpty();
+class Solution{
+	
+	function isValid(string $s)
+	{
+	    if (strlen($s) % 2 !== 0) {
+	        return false;
+	    }
+	
+	    $stack = new SplStack();
+	    $map = [
+	        ')' => '(',
+	        ']' => '[',
+	        '}' => '{',
+	    ];
+	
+	    for ($i = 0; $i < strlen($s); $i++) {
+	        $char = $s[$i];
+	
+	        if (in_array($char, ['(', '[', '{'])) {
+	            $stack->push($char);
+	        } else if (in_array($char, [')', ']', '}'])) {
+	            if ($stack->isEmpty() || $stack->pop() !== $map[$char]) {
+	                return false;
+	            }
+	        }
+	    }
+	
+	    return $stack->isEmpty();
+	}
 }
-?>
+
+$solution = new Solution();
+
+var_dump($solution->isValid("()"));
+var_dump($solution->isValid("()[]{}"));
+var_dump($solution->isValid("(]"));
+var_dump($solution->isValid("([])"));
+var_dump($solution->isValid("([)]"));
+var_dump($solution->isValid("{[]}"));
+var_dump($solution->isValid(""));
+var_dump($solution->isValid("((("));
+var_dump($solution->isValid(")))"));
+var_dump($solution->isValid("([{}])"));
